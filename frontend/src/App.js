d832bc70548b48166f02b6c080b69c025b06a655
ramanuj2112 <152617675+ramanuj2112@users.noreply.github.com>
2025-03-28 15:14:43 +0100

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EnhancedAccountUI from './EnhancedAccountUI'; // Ensure path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <EnhancedAccountUI />
+    </div>
+  );
+}
+
+export default App;
